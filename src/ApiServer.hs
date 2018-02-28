8e4f035d80cbc1d83cdafc733a210d0c4342ce35
Julian Becker <becker.julian@gmail.com>
2018-02-28 23:06:49 +0100

    Simple JSON serialization

--- src/ApiServer.hs
+++ src/ApiServer.hs
@@ -10,11 +10,14 @@ module ApiServer
 import Servant
 import Network.Wai.Handler.Warp (run)
 import Data.Text.Lazy (Text)
+import Data.ByteString.Lazy.Char8 (pack)
 import Data.Text.Lazy.Encoding (encodeUtf8)
 import Network.HTTP.Media ((//), (/:))
 import ApiServer.Middleware (middleware)
+import Data.Aeson (ToJSON, toJSON, Value(..), encode)
 
 data HTML
+data JsonDoc
 
 instance MimeRender HTML Text where
   mimeRender _ = encodeUtf8
@@ -22,13 +25,32 @@ instance MimeRender HTML Text where
 instance Accept HTML where
   contentType _ = "text" // "html" /: ("charset", "utf-8")
 
+
+
+instance MimeRender JsonDoc Value where
+  mimeRender _ = encode
+
+instance Accept JsonDoc where
+  contentType _ = "application" // "json" /: ("charset", "utf-8")
+
+
+data MyData = MyData
+  { content :: String
+  } deriving (Show)
+
+
+instance ToJSON MyData where
+    toJSON  (MyData c) = toJSON c
+
 type ServerAPI =
         Get '[HTML] Text
-  :<|>  "symbols" :> Post '[HTML] Text
+  :<|>  "symbols" :> Post '[JSON] MyData
 
+symbolsHandler :: MyData
+symbolsHandler = MyData "Hello from upload"
 
 serverRoutes :: Server ServerAPI
-serverRoutes = return "Hello from my API!" :<|> return "Hello from upload"
+serverRoutes = return "Hello from my API!" :<|> return symbolsHandler
 
 serverProxy :: Proxy ServerAPI
 serverProxy = Proxy
